d02211902997e82f7a3a56f89f200fb620e0f122
Fred Feuerbüchse <tmgaertner@googlemail.com>
2018-01-03 13:37:58 +0100

    added checks

--- Source/HarmonyPatchesAlien.cs
+++ Source/HarmonyPatchesAlien.cs
@@ -1,4 +1,6 @@
-﻿namespace AlienFaces
+﻿using System;
+
+namespace AlienFaces
 {
     using System.Reflection;
 
@@ -13,17 +15,45 @@
     using global::AlienRace;
 
     [StaticConstructorOnStartup]
-   public static class HarmonyPatchesAlien
+    public static class HarmonyPatchesAlien
     {
+        private static bool modCheck;
+        private static bool loadedAliens;
+
         static HarmonyPatchesAlien()
         {
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.alienface.patches");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
 
-            harmony.Patch(
-                AccessTools.Method(typeof(HarmonyPatchesFS), nameof(HarmonyPatchesFS.OpenStylingWindow)),
-                new HarmonyMethod(typeof(HarmonyPatchesAlien), nameof(OpenFSDialog_Prefix)),
-                null);
+            if (!modCheck)
+            {
+                loadedAliens = false;
+                foreach (ModContentPack ResolvedMod in LoadedModManager.RunningMods)
+                {
+                    if (loadedAliens) break; //Save some loading
+                    if (ResolvedMod.Name.Contains("Humanoid Alien Races"))
+                    {
+                        Log.Message("AF :: Aliens Detected.");
+                        loadedAliens = true;
+                    }
+                }
+                modCheck = true;
+            }
+
+            if (loadedAliens)
+            {
+                try
+                {
+                    harmony.Patch(
+                    AccessTools.Method(typeof(HarmonyPatchesFS), nameof(HarmonyPatchesFS.OpenStylingWindow)),
+                    new HarmonyMethod(typeof(HarmonyPatchesAlien), nameof(OpenFSDialog_Prefix)),
+                    null);
+
+                }
+                catch (Exception e)
+                {
+                }
+            }
         }
 
         public static bool OpenFSDialog_Prefix(Pawn pawn)
